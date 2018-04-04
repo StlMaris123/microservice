21ffee26bae78b4fb99ba8a5a95a17a7b3f8334b
StlMaris123 <stellamarisnjage@gmail.com>
2018-04-04 20:28:50 +0300

    implement create action

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,19 @@
 class NotificationsController < ApplicationController
+  def create
+    @notification = Notification.new(notification_params)
+    if @notification.save
+      render json: @notification, status: :created
+    else
+      render json: @notification.errors, status: :unprocessable_entity
+      #do something
+    end
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:body, :phone, :source_app)
+
+  end
+
 end
