5241e5151d5acc9727750bb8cf4fde1cac5e5be6
StlMaris123 <stellamarisnjage@gmail.com>
2018-04-05 19:07:53 +0300

    implement create using respond_to

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,19 @@
 class NotificationsController < ApplicationController
   def create
+    # @notification = Notification.new(notification_params)
+    # if @notification.save
+    #   render json: @notification, status: :created
+    # else
+    #   render json: @notification.errors, status: :unprocessable_entity
+    #   #do something
+    # end
     @notification = Notification.new(notification_params)
-    if @notification.save
-      render json: @notification, status: :created
-    else
-      render json: @notification.errors, status: :unprocessable_entity
-      #do something
+    respond_to do |format|
+      if @notification.save
+        format.json { render action: 'show', status: :created, location: @notification }
+      else
+        format.json { render json: @notification.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -13,7 +21,6 @@ class NotificationsController < ApplicationController
 
   def notification_params
     params.require(:notification).permit(:body, :phone, :source_app)
-
   end
 
 end
