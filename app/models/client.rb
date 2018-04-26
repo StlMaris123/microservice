2db15031370562a6da0a7d017dae1fc81064aec7
StlMaris123 <stellamarisnjage@gmail.com>
2018-04-26 13:32:24 +0300

    uniqueness validator

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-  validates :api_key, presence: true
-  validates :source_app, presence: true
+  validates :api_key, presence: true, uniqueness: true
+  validates :source_app, presence: true, uniqueness: true
 
   has_many :notifications, foreign_key: 'source_app', primary_key: 'source_app'
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Client, type: :model do
       client = FactoryBot.build_stubbed(:client)
       expect(client).to be_valid
     end
+
+    it 'has a unique source app' do
+        first_client = Client.create(source_app: "app_name", api_key: "I384fHtD1h9XZvs4fGPJUgtt")
+        duplicate_client = Client.create(source_app: "app_name", api_key: "I384fHtD1h9XZvs4fGPJUgtt")
+        expect(duplicate_client).to_not be_valid
+    end
   end
 end
